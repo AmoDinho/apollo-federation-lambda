5b27c8d265f01e4b12ab84279fa08867416ad2e3
AmoDinho <u14284783@tuks.co.za>
2020-03-07 19:32:50 +0200

    created bookings array

--- services/bookings/src/resolvers/query.js
+++ services/bookings/src/resolvers/query.js
@@ -3,5 +3,24 @@ export const hello = (args, context) => {
 }
 
 export const GetBookings = (args, context) => {
+    const bookings = [{
+        ID: 1,
+        listingID: 1,
+        listingName: "Apollo crater",
+        customer: "Jeff Bezos",
+        date: "23-April-2056",
+        price: "34500.00",
+        size: 4
+    },
+    {
+        ID: 2,
+        listingID: 2,
+        listingName: "Sea of Tranqility",
+        customer: "Victor Chaitezvi",
+        date: "05-June-2056",
+        price: "3000233.00",
+        size: 7
+    }
+    ]
     return null
 }
